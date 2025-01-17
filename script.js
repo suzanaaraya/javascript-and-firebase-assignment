73edf184e8c02aa2ae1dcb4c6e7866c9bda5e471
suzana araya <suzanabelay10@gmail.com>
2025-01-16 23:51:28 -0700

    authentication and real date base  are covered

--- script.js
+++ script.js
@@ -19,6 +19,7 @@
 
     const btnSignup = document.getElementById("btn-signup");
     const btnSignin = document.getElementById("btn-signin");
+
     btnSignup?.addEventListener("click",(event)=>{
       event.preventDefault()
         let email = document.getElementById("email").value;
@@ -28,12 +29,64 @@
         .then(userCredential=>{ 
       
         console.log("User signed up:", userCredential.user)
-        })
+        
+        let userName = document.getElementById("name").value;
+        let userDob = document.getElementById("dob").value;
+        let userRef = firebase.database().ref('users/');
+        userRef.set({
+          name: userName,
+          dateOfBirth: userDob
+
+        }).then(()=>{
+          console.log('User data stored successfully');
+          window.location.href = 'response.html';
+        }).catch(error => {
+          console.error("Error storing user data:", error.message);
+      })
         .catch(error=>{
         console.error("Error signing up:", error.message);
-        })
-    })
+        });
+        document.getElementById("name").value ="";
+        document.getElementById("dob").value = null;
+        document.getElementById("email").value ="";
+        document.getElementById("password").value = null;
+        
+        
+       
+       });
     
+      
+
+    });
+
+     
+   
+    btnSignin?.addEventListener("click", (e)=>{
+        e.preventDefault()
+        const email =document.getElementById("email_login").value;
+        const password =document.getElementById("password_login").value;
+        // signin user
+        auth.signInWithEmailAndPassword(email, password).then((userCredential)=>{
+            console.log("User logged in:",userCredential)
+            
+            window.location.href ='nav.html';
+            
+
+        }).catch((error)=>{
+            console.error("Login error:",error)
+        })
+
+      });
+
+
+    
+  
+
+
+
+    
+     
+
 
   
 
